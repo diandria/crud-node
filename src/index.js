267e10b9715f9ae5be9f3115bb211404e39f1ba5
Diandria Xavier <57846538+diandria@users.noreply.github.com>
2020-06-01 13:46:35 -0300

    inclusão de comentarios

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require('express'); //usado na construção da rota
+const bodyParser = require('body-parser') //usado converter o body da requisição para o formato JSON
 
-const app = express();
+const app = express(); //habilita para a construção de rotas
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //converte o body da requisição para JSON
 app.use(bodyParser.urlencoded({ extended: false}));
 
+//usado unicamente para um teste visual
+app.get('/', (req, res) => {
+    res.send("OK")
+});
+
+//Recebe todas as rotas criadas pelo index.js
 require('./app/controllers/index')(app);
 
+//Servidor rodando na porta 3000
 app.listen(3000);
