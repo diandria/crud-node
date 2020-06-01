716f81c3abfe11300bb395ab47f7aa608c0dae9c
Diandria Xavier <57846538+diandria@users.noreply.github.com>
2020-06-01 13:45:10 -0300

    inclusão de comentarios

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); //usado para utilização do banco de dados
 
+// realiza a conexão com o banco de dados
 mongoose.connect('mongodb+srv://diandria:admin@crudnode-ulwza.mongodb.net/test?retryWrites=true&w=majority', {
-    keepAlive: true,
+    // configurações base do banco de dados (atualizado)
+    keepAlive: true, 
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
