a5d3fbcf4d05d2816ddb4f0d36299275b40025ed
Diandria Xavier <57846538+diandria@users.noreply.github.com>
2020-06-01 13:40:17 -0300

    inclusão de comentarios

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -1,32 +1,38 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const crypto = require('crypto');
-const mailer = require('../../modules/mailer');
+const express = require('express'); //usado na construção da rota
+const bcrypt = require('bcryptjs'); //usado para criptografar  a senha do usuario
+const jwt = require('jsonwebtoken'); //usado para gerar o token de autenticação
+const crypto = require('crypto'); //usado para criação do token de reset de senha
+const mailer = require('../../modules/mailer'); //usado para integração do envio de email
 
-const authConfig = require('../../config/auth.json')
+const authConfig = require('../../config/auth.json') //hash secret utilizado no JWT
 
-const User = require('../models/user');
+const User = require('../models/user'); //Parte de Usuários do banco de dados
 
-const router = express.Router();
+const router = express.Router(); //Inicializa a rota
 
+//Gera um token de autenticação atraves de hash quando o usuario se loga por 24h
 function genereteToken(params = {}) {
     return jwt.sign(params, authConfig.secret, {
         expiresIn: 86400,
     });
 }
 
+//Rota de crição de usuario
 router.post('/register', async (req, res) => {
     const { email } = req.body;
 
     try {
+        //verifica se o usuario ja existe atraves do email
         if (await User.findOne({ email }))
             return res.status(400).send({ error: 'User already exists' });
 
+        //cria o usuario
         const user = await User.create(req.body);
 
+        //não exibe a senha no momento da response
         user.password = undefined;
     
+        //retorna os dados do usuario criado
         return res.send({ 
             user,
             token: genereteToken({ id: user.id}),
@@ -36,23 +42,29 @@ router.post('/register', async (req, res) => {
     }
 });
 
+//Rota de autenticação do usuário
 router.post('/authenticate', async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email }).select('+password');
 
+    //verifica se o usuario ja existe atraves do email
     if (!user)
         return res.status(400).send({ error: 'User not found' });
 
+    //verifica se a senha está correta
     if (!await bcrypt.compare(password, user.password))
         return res.status(400).send({ error: 'Invalid password' });
 
+    //não exibe a senha no momento da response
     user.password = undefined;
     
+    //verifica o token de autenticação do usuario
     const token = jwt.sign({ id: user.id }, authConfig.secret, {
         expiresIn: 86400,
     });
 
+    //retorna os dados do usuario autenticado
     res.send({ 
         user, 
         token: genereteToken({ id: user.id}),
@@ -60,20 +72,25 @@ router.post('/authenticate', async (req, res) => {
 
 });
 
+//Rota "esqueci minha senha"
 router.post('/forgot_pass', async (req, res) => {
     const { email } = req.body;
 
     try {
         const user = await User.findOne( { email });
 
+        //verifica se o usuario ja existe atraves do email
         if (!user)
             return res.status(400).send({ error: 'User not found' });
         
+        //gera um token que será usado para recuperação de senha
         const token = crypto.randomBytes(20).toString('hex');
 
+        //pega o momento atual e adiciona 1h para o token expirar, o usuario só poderá resetar a senha nesse intervalo de hora
         const now = new Date();
         now.setHours(now.getHours() + 1);
 
+        //encontra o usuario e insere o token e o tempo de expiração do token no banco de dados
         await User.findOneAndUpdate(user.id, {
             '$set': {
                 passwordResetToken: token,
@@ -81,6 +98,7 @@ router.post('/forgot_pass', async (req, res) => {
             }
         });
         
+        //envia o token para o email do usuario
         mailer.sendMail({
             to: email,
             from: 'diandriaferreira@gmail.com',
@@ -96,6 +114,7 @@ router.post('/forgot_pass', async (req, res) => {
     }
 });
 
+//Rota que atualiza a senha no banco de dados
 router.post('/reset_pass', async (req, res) => {
     const { email, token, password } = req.body
 
@@ -103,19 +122,23 @@ router.post('/reset_pass', async (req, res) => {
         const user = await User.findOne( { email } )
             .select('+passwordResetToken passwordResetExpires');
 
+        //verifica se o usuario ja existe atraves do email
         if (!user)
             return res.status(400).send({ error: 'User not found' });
         
+        //verifica se o token informado é válido
         if (token !== user.passwordResetToken)
             return res.status(400).send({ error: 'Token invalid' });
-            
+        
+        //verifica se o token ainda esta válido
         const now = new Date();
-
         if (now > user.passwordResetExpires)
             return res.status(400).send({ error: 'Token expired, generated a new one' });
 
+        //define a nova senha
         user.password = password;
 
+        //atualiza a senha no banco de dados
         await user.save();
 
         res.send();
@@ -125,4 +148,5 @@ router.post('/reset_pass', async (req, res) => {
     }
 });
 
+//Rota base de autorização localhost:3000/auth/<nomeDaRota>
 module.exports = app => app.use('/auth', router);
